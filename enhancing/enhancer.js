3d2a8851ccecba20a60f1ea004d7592befca5051
Marco Whyte <mwhyte@umass.edu>
2020-03-26 15:27:08 -0500

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,53 @@
 module.exports = {
   succeed,
   fail,
-  repair,
-  get,
+  repair
 };
 
 function succeed(item) {
-  return { ...item };
+  validateItem(item);
+  let { name, enhancement, durability } = item;
+  if (enhancement < 20) {
+    enhancement++;
+  }
+  return { name, enhancement, durability };
 }
 
 function fail(item) {
-  return { ...item };
+  validateItem(item);
+  let { name, enhancement, durability } = item;
+  if (enhancement > 16) {
+    enhancement = enhancement - 1;
+  }
+  if (enhancement > 15) {
+    durability = durability - 10;
+  } else {
+    durability = durability - 5;
+  }
+  return { name, enhancement, durability };
 }
 
 function repair(item) {
-  return { ...item };
+  validateItem(item);
+  let { durability } = item;
+  durability = 100;
+  return { ...item, durability };
 }
 
-function get(item) {
-  return { ...item };
+function validateItem(item) {
+  if (!item.name || item.name === "") {
+    throw new Error("Must give item name");
+  } else if (
+    typeof item.durability !== "number" ||
+    item.durability < 0 ||
+    item.durability > 100
+  ) {
+    throw new Error("Must have item number 0-100 for durability");
+  } else if (
+    typeof item.enhancement !== "number" ||
+    item.enhancement < 0 ||
+    item.enhancement > 20
+  ) {
+    throw new Error("Must have enhancement number 0-20 for enhancement.");
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,85 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer repair function", () => {
+  describe("repair function", () => {
+    it("only accepts an item object", () => {
+      const emptyObject = {};
+      expect(() => {
+        enhancer.repair(emptyObject);
+      }).toThrow();
+    });
+    it("returns an item object with 100 durability", () => {
+      const item = {
+        name: "Item",
+        durability: 20,
+        enhancement: 0
+      };
+      expect(enhancer.repair(item).durability).toBe(100);
+    });
+  });
+  describe("success function", () => {
+    it("Only works with item object", () => {
+      const emptyObject = {};
+      expect(() => {
+        enhancer.succeed(emptyObject);
+      }).toThrow();
+    });
+    it("item's enhancement + 1", () => {
+      const item = {
+        name: "Item",
+        durability: 10,
+        enhancement: 0
+      };
+      expect(enhancer.succeed(item).enhancement).toBe(1);
+    });
+    it("Item enhancement capped at 20", () => {
+      const item = {
+        name: "Item",
+        durability: 5,
+        enhancement: 20
+      };
+      expect(enhancer.succeed(item).enhancement).toBe(20);
+    });
+    it("won't change the item's durability", () => {
+      const item = {
+        name: "Item",
+        durability: 5,
+        enhancement: 20
+      };
+      expect(enhancer.succeed(item).durability).toBe(5);
+    });
+  });
+  describe("fail function", () => {
+    it("only accepts an item object", () => {
+      const emptyObject = {};
+      expect(() => {
+        enhancer.fail(emptyObject);
+      }).toThrow();
+    });
+    it("if enhancement level is < 15 durability - 5", () => {
+      const item = {
+        name: "Item",
+        durability: 40,
+        enhancement: 10
+      };
+      expect(enhancer.fail(item).durability).toBe(35);
+    });
+    it("if enhancement level is > 15 durability - 10", () => {
+      const item = {
+        name: "Item",
+        durability: 50,
+        enhancement: 20
+      };
+      expect(enhancer.fail(item).durability).toBe(40);
+    });
+    it("if enhancement level is > 16 enhancement - 1", () => {
+      const item = {
+        name: "Item",
+        durability: 50,
+        enhancement: 20
+      };
+      expect(enhancer.fail(item).enhancement).toBe(19);
+    });
+  });
+});
